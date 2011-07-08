1ce38f98744068f1949b90a346dbddaa21ee2d33
Andre Nathan <andre@digirati.com.br>
2011-07-08 11:48:45 -0300

    Clean before compiling

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,8 @@
 
 d=`dirname $0`
 echo $d
-cd $d/mycat && gomake && cd -
-cd $d/openfile && gomake && cd -
+cd $d/mycat && gomake clean && gomake && cd -
+cd $d/openfile && gomake clean && gomake && cd -
 
 cd $d
 ./mycat/mycat ./mycat/mycat.go
